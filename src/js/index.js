3e34696aee6419487a81d1ca098a85b1cfe67696
stephaniealbarran <stefanyalbarran@hotmail.com>
2022-09-05 19:06:07 +0000

    hecho

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,11 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
+//include your index.scss file into the bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Home } from "./component/home.js";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
